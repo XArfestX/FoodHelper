476479e293191388c56e6dea505c2a9b761419f3
XArfestX <Arfest2001@gmail.com>
2022-09-27 15:56:27 +0300

    Add DB files

---
+++ app/src/main/java/com/app/foodhelper/db/MealDao.kt
@@ -0,0 +1,18 @@
+package com.app.foodhelper.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.app.foodhelper.pojo.Meal
+
+@Dao
+interface MealDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(meal: Meal)
+
+    @Delete
+    suspend fun delete(meal: Meal)
+
+    @Query("SELECT * FROM mealInformation")
+    fun getAllMeals(): LiveData<List<Meal>>
+
+}
